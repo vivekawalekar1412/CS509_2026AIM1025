ab33c1724a636433019d5c80f42e18d8e90868b1
vivekawalekar1412 <2026aim1025@iit.ac.in>
2026-08-18 23:32:30 +0530

    Add Assignment 3 MST headers

---
+++ ASSIGNMENT_3/include/mst.h
@@ -0,0 +1,23 @@
+#ifndef MST_H
+#define MST_H
+
+#include "csr.h"
+
+typedef struct {
+    int u;
+    int v;
+    int weight;
+} Edge;
+
+typedef struct {
+    Edge *edges;
+    int count;
+    long long total_weight;
+    double execution_time_ms;
+} MSTResult;
+
+MSTResult run_kruskal(const CSRGraph *csr);
+MSTResult run_prim(const CSRGraph *csr);
+void free_mst_result(MSTResult *res);
+
+#endif // MST_H
